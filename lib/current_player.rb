1362e81c563e5be1b7f171c27d81f515c62226b3
Brian Ringley <brian.ringley@wework.com>
2018-02-19 14:03:45 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,9 @@
+# SINGLE RESPONSIBILITY PRINCIPLE! I love it 
+# Breaking down the behavior of our program into its smallest possible constituents gives us the flexibility we need to take those building blocks and arrange them into new configurations as new goals and problems arrive.
+
+def turn_count(board)
+  board.each do |board_space|
+end
+
+def current_player
+end
