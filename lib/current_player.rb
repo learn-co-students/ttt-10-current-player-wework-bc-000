f66c9e3d84eba32cbfbe66df34aacc61941aaa6a
Brian Ringley <brian.ringley@wework.com>
2018-02-19 18:56:49 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,17 @@
 # Breaking down the behavior of our program into its smallest possible constituents gives us the flexibility we need to take those building blocks and arrange them into new configurations as new goals and problems arrive.
 
 def turn_count(board)
+  spaces_taken = 0
   board.each do |board_space|
+    if board_space == "X" || board_space == "O"
+      spaces_taken += 1 
+    end
 end
 
-def current_player
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    puts "X"
+  else
+    puts "O"
+  end
 end
